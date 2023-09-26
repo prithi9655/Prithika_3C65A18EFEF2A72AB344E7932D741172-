f4ad991ec9f37f73e361bde934079100a5792996
prithi9655 <144683750+prithi9655@users.noreply.github.com>
2023-09-26 16:34:27 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,19 @@
+#Define the base class player
+class player:
+  def play(self):
+    print ("The player is playing cricket")
+  #define the derived class Batsman
+class Batsman( player):
+  def Play (self):
+    print("The batsman is batting")
+  # Define the derived class bowler
+class Bowler(player):
+  def play(self):
+    print ("The bowler is bowling")
+    # Create object of Batsman and Bowler classes
+batsman=Batsman ()
+bowler=Bowler ()
+#call the play() method foreach object
+batsman.play()
+bowler.play()
+  
